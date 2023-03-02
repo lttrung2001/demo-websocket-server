22d105163595ba2c6233997a5931fddb3e4b4143
unknown <lt.trung2001@gmail.com>
2023-03-02 22:00:10 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/configs/MyWebSocketHandler.java
@@ -0,0 +1,57 @@
+package com.example.demo.configs;
+
+import com.example.demo.models.Payload;
+import com.google.gson.Gson;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyWebSocketHandler extends AbstractWebSocketHandler {
+    private final Map<String, List<WebSocketSession>> topics = new HashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        List<WebSocketSession> sessions = topics.get("global");
+        if (sessions == null) {
+            sessions = new ArrayList<>();
+        }
+        sessions.add(session);
+        topics.put("global", sessions);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+        List<WebSocketSession> sessions = topics.get("global");
+        sessions.remove(session);
+        System.out.println("Number of connections: " + sessions.size());
+        if (sessions.size() > 0) {
+            sessions.forEach(s -> {
+                try {
+                    s.sendMessage(new TextMessage(session.getId() + "has been disconnected."));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        Payload payload = new Gson().fromJson(message.getPayload(), Payload.class);
+        String msg = payload.getMessage();
+
+        System.out.println(msg);
+        for (WebSocketSession s : topics.get("global")) {
+            s.sendMessage(new TextMessage(session.getId() + ": " + msg));
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/configs/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new MyWebSocketHandler(), "/websocket")
+                .setHandshakeHandler(new DefaultHandshakeHandler(new TomcatRequestUpgradeStrategy())).setAllowedOrigins("*");
+    }
+}
