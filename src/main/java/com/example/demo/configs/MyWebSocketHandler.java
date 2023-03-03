24861d2a615cda596c3da4ab44cacb89cd702fc8
unknown <lt.trung2001@gmail.com>
2023-03-03 13:12:45 +0700

    Update MyWebSocketHandler.java

--- src/main/java/com/example/demo/configs/MyWebSocketHandler.java
+++ src/main/java/com/example/demo/configs/MyWebSocketHandler.java
@@ -2,12 +2,14 @@ package com.example.demo.configs;
 
 import com.example.demo.models.Payload;
 import com.google.gson.Gson;
+import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -52,6 +54,17 @@ public class MyWebSocketHandler extends AbstractWebSocketHandler {
         System.out.println(msg);
         for (WebSocketSession s : topics.get("global")) {
             s.sendMessage(new TextMessage(session.getId() + ": " + msg));
+        }
+    }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+        Thread.sleep(200);
+        super.handleBinaryMessage(session, message);
+        ByteBuffer byteBuffer = message.getPayload();
+        System.out.println(byteBuffer);
+        for (WebSocketSession s : topics.get("global")) {
+            s.sendMessage(message);
         }
     }
 }
